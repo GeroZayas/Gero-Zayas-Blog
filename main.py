5bc76140db9614a3dbb714532e77fabb474ff2e4
Gero <gerozayas@gmail.com>
2021-03-16 12:39:47 +0100

    change os.environ secret key

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
-import os
 
 # NOTICE I'm importing os, and then using os.environ for the env variables I'm protecting here
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = 'SECRET_KEY'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
